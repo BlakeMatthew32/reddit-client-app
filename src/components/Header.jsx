31fb1d604b11ab5ea38ed0298298a73baaf40aa2
Blake Matthew <79722560+BlakeMatthew32@users.noreply.github.com>
2025-06-25 22:51:46 +0100

    feat: add basic features to the header including search input

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,18 @@
+import { useState } from "react"
 
 const Header = () => {
+  const [searchInput, setSearchInput] = useState("")
+
+  const handleChange = ({target}) => {
+    setSearchInput(target.value)
+  }
+
   return (
     <header>
       <h2>Reddit Client App</h2>
+      <label htmlFor="search">
+        <input name='search' id="search" placeholder="Search" onChange={handleChange} value={searchInput} />
+      </label>
     </header>
   )
 }
